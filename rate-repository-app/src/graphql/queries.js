453d63d6d14f40d3f243fcb9d355bea9d8b8bc1b
josh <joshaubrey7@gmail.com>
2022-01-23 14:49:08 -0600

    change useRepositories hook from fetch to graphql query

--- rate-repository-app/src/graphql/queries.js
+++ rate-repository-app/src/graphql/queries.js
@@ -1,11 +1,16 @@
 import { gql } from '@apollo/client';
+import { REPOSITORY_DETAILS } from "./fragments";
 
 export const GET_REPOSITORIES = gql`
   query {
     repositories {
-      ${/* ... */}
+      totalCount
+      edges {
+        node {
+          ...RepositoryDetails
+        }
+      }
     }
   }
+  ${REPOSITORY_DETAILS}
 `;
-
-// other queries...
